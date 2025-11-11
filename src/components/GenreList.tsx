7695d098e14c53299ef05925fdc36930cc7d43f2
Waseem Abufares <laptopw4seem@gmail.com>
2025-11-11 11:27:02 +0300

    spinner while waiting

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,10 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { dataa } = useGenres();
+  const { dataa, isLoading, error } = useGenres();
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <List>
       {dataa.map((genre) => (
